9ea887a88eaf7cb082d1a4c66d0ca5d03818aed6
Jian Min Soh <115980621+jianmin22@users.noreply.github.com>
2024-03-26 22:54:23 +0800

    fix npm run build error

--- src/app/my_experience/timeline/page.tsx
+++ src/app/my_experience/timeline/page.tsx
@@ -23,7 +23,7 @@ export default function Timeline() {
                                 <div className="h-36 w-full m-5 relative">
                                     <img src="../../images/sleep.png" alt="Linkedin" className="absolute inset-0 w-full h-full object-contain" />
                                 </div>
-                                <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
                         </div>
 
@@ -34,7 +34,7 @@ export default function Timeline() {
                             </div>
                             <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
                                 <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-                                <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
                         </div>
 
@@ -45,7 +45,7 @@ export default function Timeline() {
                             </div>
                             <div className="order-1 bg-gray-200 rounded-lg shadow-xl w-5/12 px-6 py-4">
                                 <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-                                <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
                         </div>
 
@@ -56,7 +56,7 @@ export default function Timeline() {
                             </div>
                             <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
                                 <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-                                <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
                         </div>
                     </div>
